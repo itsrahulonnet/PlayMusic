7d4a179b8bd3d7bfd68bf8c926ed581fca9d2a7c
rjain <itsrahulonnet@gmail.com>
2014-02-24 13:28:45 -0600

    Added method to retrieve media files from SD Card

--- app/src/main/java/com/example/playmusic/app/MainActivity.java
+++ app/src/main/java/com/example/playmusic/app/MainActivity.java
@@ -1,16 +1,30 @@
 package com.example.playmusic.app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.database.Cursor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.IOException;
+import java.util.HashMap;
+
 public class MainActivity extends ActionBarActivity {
+
+    private HashMap<Long,String> mHashMap = new HashMap<Long, String>();
+    private MediaPlayer mMediaPlayer = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);     
+        setContentView(R.layout.activity_main);
+        getAllMediaFiles();
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
     }
 
 
@@ -34,4 +48,35 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void getAllMediaFiles(){
+        ContentResolver contentResolver = getContentResolver();
+        Uri uri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor cursor = contentResolver.query(uri, null, null, null, null);
+        if (cursor == null) {
+            // query failed, handle error.
+        } else if (!cursor.moveToFirst()) {
+            // no media on the device
+        } else {
+            int titleColumn = cursor.getColumnIndex(android.provider.MediaStore.Audio.Media.TITLE);
+            int idColumn = cursor.getColumnIndex(android.provider.MediaStore.Audio.Media._ID);
+            do {
+                long thisId = cursor.getLong(idColumn);
+                String thisTitle = cursor.getString(titleColumn);
+                mHashMap.put(thisId,thisTitle);
+            } while (cursor.moveToNext());
+        }
+    }
+
+    private void playSongs(int index){
+        Uri contentUri = ContentUris.withAppendedId(
+                android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, index);
+        mMediaPlayer.reset();
+        try{
+            mMediaPlayer.setDataSource(getApplicationContext(), contentUri);
+            mMediaPlayer.prepare();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        mMediaPlayer.start();
+    }
 }
